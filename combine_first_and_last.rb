21e35ec965734dc3f19890bc3433c64b6cd0574f
Decabits Learning Management System <sikand.jasmeet3@gmail.com>
2019-07-09 19:55:57 +0530

    Final

--- combine_first_and_last.rb
+++ combine_first_and_last.rb
@@ -3,9 +3,15 @@ def combine_first_and_last(key_value_pairs)
     keys = key_value_pairs.keys
     values  = key_value_pairs.values
     new_key = keys.first.to_s << keys.last.to_s
-    a = key_value_pairs.values.first
-    b = key_value_pairs.values.to_a.last
-    new_value = a + b
+    if values[0].class == Array
+      a = key_value_pairs.values.first
+      b = key_value_pairs.values.to_a.last
+      new_value = a + b
+    else
+      a = key_value_pairs.values.first.to_s
+      b = key_value_pairs.values.last.to_s
+      new_value = a + b
+    end
     key_value_pairs.delete(keys.first)
     key_value_pairs.delete(keys.last)
     new_keys = {new_key => new_value}.merge(key_value_pairs)
